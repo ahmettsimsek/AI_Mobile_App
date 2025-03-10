50de705c3c50d24d4c15e6296e58453f4704c68a
Ahmtsmsek7 <ahmtsmsek7@gmail.com>
2025-03-10 10:53:49 +0300

    commit

---
+++ screen/sss.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const sss = () => {
+  return (
+    <View>
+      <Text>sss</Text>
+    </View>
+  )
+}
+
+export default sss
